c046ee139878fed65f501182125e8f766b7a2abe
Malekith101991 <j.gutierrezv08@gmail.com>
2022-10-31 09:54:20 -0500

    Se suben cambios

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "Juanma.1991",
     "database": "amigo_secreto",
     "host": "127.0.0.1",
     "dialect": "mysql"
